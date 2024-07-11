fa0555114b02597baf417b288d2e859fdb7e67d1
lixy115 <lixy115@lenovo.com>
2024-07-11 18:16:50 +0800

    init

---
+++ apps/react-router/src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./routes/errorPage";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children:[
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+                errorElement: <ErrorPage />,
+            },
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
